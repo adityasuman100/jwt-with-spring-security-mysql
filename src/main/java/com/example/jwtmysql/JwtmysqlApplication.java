2f4ced274c907bb2f21eeb741e0a232ed4b99d98
Aditya <adityasuman100@gmail.com>
2024-02-28 17:04:42 +0530

    initial

---
+++ src/main/java/com/example/jwtmysql/JwtmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtmysqlApplication.class, args);
+	}
+
+}
